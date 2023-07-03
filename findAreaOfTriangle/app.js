ba81055b6f59ea7475fa92d6c4b65a5de124e3fc
TanzeelUrRehman1995 <tanzeelurrehman1995@gmail.com>
2023-07-03 21:39:06 +0500

    area of triangle

---
+++ findAreaOfTriangle/app.js
@@ -0,0 +1,8 @@
+var base = window.prompt("Enter the base triangle", "");
+var height = window.prompt("Enter the height of triangle", "");
+
+console.log(`The Base of triangle is ${base}`);
+console.log(`The Height of triangle is ${height}`);
+
+ var areaOfTriangle = (base*height)/2;
+console.log(`The Area of triangle is ${areaOfTriangle}`)
