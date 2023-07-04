e9d073f18e8c644b39e5217a7d1a8bbb4fdfd890
TanzeelUrRehman1995 <tanzeelurrehman1995@gmail.com>
2023-07-04 11:47:44 +0500

    js programs
    multiple programs in one first which is commented

---
+++ js multiple programs/app.js
@@ -0,0 +1,73 @@
+/*var input1 = window.prompt("Enter 1st value", "");
+var input2 = window.prompt("Enter 2ndvalue", "");
+var input3 = window.prompt("Enter 3rd value", "");
+var input4 = window.prompt("Enter 4th value", "");
+var input5 = window.prompt("Enter 5th value", "");
+
+console.log(`1st Value = ${input1}`);
+console.log(`2nd Value = ${input2}`);
+console.log(`3th Value = ${input3}`);
+console.log(`4th Value = ${input4}`);
+console.log(`5th Value = ${input1}`);
+*/
+
+/*var arr = [];
+arr[0] =window.prompt("Enter a number : ")
+arr[1] =window.prompt("Enter a number : ")
+arr[2] =window.prompt("Enter a number : ")
+arr[3] =window.prompt("Enter a number : ")
+arr[4] =window.prompt("Enter a number : ") 
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+*/
+
+/*var arr = [ "A", "B" , "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W","X", "Y", "Z"];
+for (var i = 0; i <26; i++)
+{
+console.log(arr[i]);
+}
+*/
+
+/*
+for (var i = 65 ; i <= 90 ; i++)
+{
+console.log(String.fromCharCode(i));
+}
+*/
+
+/*arr = [];
+for (var z = 0; z < 26 ; z++)
+{
+arr[z] = String.fromCharCode(z + 65) 
+}
+console.log(arr);
+*/
+
+/*var capitalLetters = "";
+
+for (var z = 0; z < 26 ; z++)
+{
+capitalLetters = capitalLetters + String.fromCharCode(z + 65); 
+capitalLetters += " ";
+}
+
+console.log(capitalLetters);
+*/
+
+
+// var capitalLetters = "";
+
+// for (var k = 0; k < 26 ; k++)
+// {
+// capitalLetters = capitalLetters + String.fromCharCode(k + 65); 
+// // capitalLetters += String.fromCharCode(k + 65)
+// }
+
+// console.log(capitalLetters[Math.floor(Math.random() * capitalLetters.length)]);
+
+var num = Math.random();
+console.log(num);
